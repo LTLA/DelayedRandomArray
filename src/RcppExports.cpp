46c3eca1c098e42208d0ce87cd72a9e2f5e30be4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-07-06 00:25:00 -0700

    Minor fixes for roxygen, added ignore file.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // recycle_vector
 Rcpp::NumericVector recycle_vector(Rcpp::NumericVector vec, Rcpp::IntegerVector dim, Rcpp::List index);
 RcppExport SEXP _DelayedRandomArray_recycle_vector(SEXP vecSEXP, SEXP dimSEXP, SEXP indexSEXP) {
